0d5426359aa138038ac5e271601854e7c07ca63f
Michael Williams <dinosaur@riseup.net>
2015-04-13 19:09:07 +1200

    update vocabulary to reflect current data, implement some relations dream code

--- vocab/Agent.js
+++ vocab/Agent.js
@@ -1,38 +1,90 @@
 module.exports = {
-  id: "Agent",
-  type: 'object',
-  prefixes: {
-    "": "http://schema.org/",
-    "foaf": "http://xmlns.com/foaf/0.1/", 
-  },
-  context: "foaf:Agent",
-  description: "An agent.",
+  id: 'Agent',
+  collection: 'agents',
+  prefixes: require('./prefixes'),
+  context: 'foaf:Agent',
+  description: 'A thing with the power to act.',
   properties: {
     name: {
-      context: "name",
-      description: "The primary name of the agent.",
-      type: "string",
+      context: 'schema:name',
+      description: 'The primary name of the agent.',
+      type: 'string'
     },
     description: {
-      context: "description",
-      description: "A short description of the agent.",
-      type: "string",
+      context: 'schema:description',
+      description: 'A short description of the agent.',
+      type: 'string'
+    },
+    image: {
+      context: 'schema:image',
+      description: 'An avatar of the agent.',
+      type: 'string',
+      format: 'uri'
+    },
+    roles: {
+      context: 'hol:hasRole',
+      description: 'The roles the agent is in.',
+      type: 'array',
+      items: {
+        $ref: 'Role'
+      },
+      get: {
+        deps: ['$roles'],
+        fn: function (allRoles) {
+          return allRoles
+          .find({
+            path: ['agent'],
+            eq: this.id
+          })
+        }
+      }
     },
     relationships: {
-      context: "ovn:hasRelationship",
-      description: "The relationships the agent has or is with other agents.",
-      type: "array",
+      context: 'hol:hasRelationship',
+      description: 'The relationships the agent has with other agents.',
+      type: 'array',
       items: {
-        anyOf: [{
-          reverse: "has",
-        }, {
-          reverse: "is",
-        }]
-        $ref: "Relationship",
+        $ref: 'Relationship'
       },
+      get: {
+        deps: ['@roles'],
+        fn: function (myRoles) {
+          return myRoles
+          .map(function (role) {
+            return role.relationship
+          })
+          .filter(function (rel) {
+            return rel
+          })
+        }
+      }
     },
-  },
-};
-module.exports.dependencies = {
-  Relationship: require('./Relationship'),
-};
+    groups: {
+      context: 'schema:memberOf',
+      description: 'The groups the agent is a member of.',
+      type: 'array',
+      items: {
+        $ref: 'Agent'
+      },
+      get: {
+        deps: ['@relationships'],
+        fn: function (myRelationships) {
+          // from this agent's relationships
+          return myRelationships
+          // that have relationship type with name 'membership',
+          .find({
+            path: ['type', 'name'],
+            eq: 'membership'
+          })
+          // return agent in group role
+          .map(function (rel) {
+            return rel.roles.findOne({
+              path: ['type', 'name'],
+              eq: 'group'
+            }).get('agent')
+          })
+        }
+      }
+    }
+  }
+}
--- vocab/Relationship.js
+++ vocab/Relationship.js
@@ -1,50 +1,37 @@
 module.exports = {
-  id: "Relationship",
-  type: 'object',
-  prefixes: {
-    "": "http://schema.org/",
-    "dex": "http://vocab.craftodex.is/",
-  },
-  description: "A relationship between multiple agents.",
-  context: "dex:Relationship",
+  id: 'Relationship',
+  collection: 'relationships',
+  prefixes: require('./prefixes'),
+  description: 'A relationship between multiple agents.',
+  context: 'hol:Relationship',
   properties: {
-    relationshipType: {
-      description: "The type of agent relationship.",
-      context: "dex:relationshipType",
-      $ref: "RelationshipType",
+    type: {
+      description: 'The type of agent relationship.',
+      context: 'hol:relationshipType',
+      $ref: 'RelationshipType'
     },
     description: {
       description: "A description of the agents' relations.",
-      context: "description",
-      type: "string",
+      context: 'schema:description',
+      type: 'string'
     },
-    is: {
-      description: "The agent that is <relationship name>.",
-      context: "dex:isRelated",
-      $ref: "Agent",
-    },
-    has: {
-      description: "The agent that has <relationship name>.",
-      context: "dex:hasRelated",
-      $ref: "Agent",
-    },
-    status: {
-      description: "The status of the relationship.",
-      context: "dex:status",
-      enum: [
-        "active",
-        "inactive",
-        "potential",
-      ],
-    },
-    symmetric: {
-      description: "The symmetric relationship that corresponds to this relationship.",
-      context: "dex:symmetric",
-      $ref: "Relationship",
-    },
-  },
-  dependencies: {
-    RelationshipType: require('./RelationshipType'),
-    Agent: require('./Agent'),
-  },
-};
+    roles: {
+      description: 'The roles within this relationship.',
+      context: 'hol:relationshipRole',
+      type: 'array',
+      items: {
+        $ref: 'Role'
+      },
+      get: {
+        deps: ['$roles'],
+        fn: function (allRoles) {
+          return allRoles
+          .find({
+            path: ['relationship'],
+            eq: this.id
+          })
+        }
+      }
+    }
+  }
+}
