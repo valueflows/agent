21b322e1da2289c651e0f49aa97c4f6c0014d499
Michael Williams <michael.williams@enspiral.com>
2015-10-11 13:53:39 +1300

    remove vocab/ directory
    closes https://github.com/valueflows/valueflows/issues/35.
    
    i plan to bring this back once i have capacity to deliver on a single
    source of truth for the vocab which is used to generate derivative
    truths.

--- vocab/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  AgentType: require('./AgentType'),
-  Agent: require('./Agent'),
-  RelationshipType: require('./RelationshipType'),
-  Relationship: require('./Relationship')
-}
--- vocab/prefixes.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  open: 'http://openvocab.is/#',
-  schema: 'http://schema.org/',
-  as: 'http://www.w3.org/ns/activitystreams#',
-  foaf: 'http://xmlns.com/foaf/0.1/',
-  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-  rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
-  owl: 'http://www.w3.org/2002/07/owl#'
-}
